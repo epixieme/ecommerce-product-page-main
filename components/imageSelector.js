b4177374b50c7cbe5145cb568cc9f521638a57e7
Kirstie <39728053+epixieme@users.noreply.github.com>
2024-09-11 15:19:09 +0100

    chore: Update imageSelector.js to include largeSrc property for images and improve thumbnail click functionality

--- components/imageSelector.js
+++ components/imageSelector.js
@@ -1,28 +1,31 @@
-export function imageSelector() {
-  const images = [
-    {
-      id: 1,
-      src: "images/image-product-1-thumbnail.jpg",
-      alt: "shoe 1",
-      src: "images/image-product-1.jpg",
-    },
-    {
-      id: 2,
-      src: "images/image-product-2-thumbnail.jpg",
-      alt: "shoe 2",
-    },
-    {
-      id: 3,
-      src: "images/image-product-3-thumbnail.jpg",
-      alt: "shoe 3",
-    },
-    {
-      id: 4,
-      src: "images/image-product-4-thumbnail.jpg",
-      alt: "shoe 4",
-    },
-  ];
+const images = [
+  {
+    id: 1,
+    src: "images/image-product-1-thumbnail.jpg",
+    alt: "shoe 1",
+    largeSrc: "images/image-product-1.jpg",
+  },
+  {
+    id: 2,
+    src: "images/image-product-2-thumbnail.jpg",
+    alt: "shoe 2",
+    largeSrc: "images/image-product-2.jpg",
+  },
+  {
+    id: 3,
+    src: "images/image-product-3-thumbnail.jpg",
+    alt: "shoe 3",
+    largeSrc: "images/image-product-3.jpg",
+  },
+  {
+    id: 4,
+    src: "images/image-product-4-thumbnail.jpg",
+    alt: "shoe 4",
+    largeSrc: "images/image-product-4.jpg",
+  },
+];
 
+export function imageSelector() {
   return `
   <div class='shoe-container'>
         <img src="images/image-product-1.jpg" width="100%" height="auto" class='shoe-image'>
@@ -41,15 +44,17 @@ export function imageSelector() {
 export function handleThumbnailClick() {
   const thumbnails = document.querySelectorAll(".shoe__thumbnail");
 
-  thumbnails.forEach((thumbnail) => {
+  thumbnails.forEach((thumbnail, i) => {
     thumbnail.addEventListener("click", () => {
-      console.log("thumbnail clicked");
       const shoeImage = document.querySelector(".shoe-image");
-      shoeImage.src = thumbnail.src;
-      thumbnails.forEach((thumbnail) =>
-        thumbnail.classList.remove("shoe--active")
-      );
-      thumbnail.classList.add("shoe--active");
+      shoeImage.src = images[i].largeSrc;
+      // Find the current active thumbnail and remove the active class
+      const activeThumbnail = document.querySelector(".shoe-image--active");
+      if (activeThumbnail) {
+        activeThumbnail.classList.remove("shoe-image--active");
+      }
+      // Add the active class to the clicked thumbnail
+      thumbnail.classList.add("shoe-image--active");
     });
   });
 }
